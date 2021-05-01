58d65c4f7a90203ed131da5f9cfac048f81756e8
Pawan JATT <76043107+jattpawan@users.noreply.github.com>
2021-05-01 11:17:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,8 +21,8 @@ COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 
-    HEROKU_API_KEY = os.environ.get("HEROKU_APIKEY", None)
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APPNAME", None)
-    HEROKU_APIKEY = os.environ.get("HEROKU_APIKEY", None)
-    HEROKU_APPNAME = os.environ.get("HEROKU_APPNAME", None)
-    UPSTREAM_REPO_URL = os.environ.get("UPSTREAM_REPO_URL","https://github.com/jattpawan/evilbot")
+HEROKU_API_KEY = os.environ.get("HEROKU_APIKEY", None)
+HEROKU_APP_NAME = os.environ.get("HEROKU_APPNAME", None)
+HEROKU_APIKEY = os.environ.get("HEROKU_APIKEY", None)
+HEROKU_APPNAME = os.environ.get("HEROKU_APPNAME", None)
+UPSTREAM_REPO_URL = os.environ.get("UPSTREAM_REPO_URL","https://github.com/jattpawan/evilbot")
